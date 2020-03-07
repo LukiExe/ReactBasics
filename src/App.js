2801125da2ee01f268883902b6bbb0256ccc9627
LukiExe <MrLuki2011@gmail.com>
2020-03-07 16:38:47 +0100

    finishedTask

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
       );
     return (
     <div className='App'>
+    <h1> Users </h1>
     <SearchBox 
     placeholder='search users'
   handleChange={this.handleChange}
