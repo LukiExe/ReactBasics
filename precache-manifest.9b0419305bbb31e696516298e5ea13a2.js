6eac3f727f7dde58d60f587bb0a7130ea692f38b
LukiExe <MrLuki2011@gmail.com>
2020-03-08 20:27:21 +0100

    Updates

---
+++ precache-manifest.9b0419305bbb31e696516298e5ea13a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb1fa8dcd4546b3702cb5d7fae25c02",
+    "url": "/ReactBasics/index.html"
+  },
+  {
+    "revision": "c32c65852216d2723aec",
+    "url": "/ReactBasics/static/css/main.270c13fd.chunk.css"
+  },
+  {
+    "revision": "7dd9aa93db2262f6fe23",
+    "url": "/ReactBasics/static/js/2.b146e6eb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactBasics/static/js/2.b146e6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c32c65852216d2723aec",
+    "url": "/ReactBasics/static/js/main.297c29c5.chunk.js"
+  },
+  {
+    "revision": "c4bfb6372cc2bf488927",
+    "url": "/ReactBasics/static/js/runtime-main.27a7fd83.js"
+  }
+]);
